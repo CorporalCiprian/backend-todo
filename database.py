2e0975642312c16ad7b97c9f43482185f5cfd713
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-07-27 14:36:36 +0300

    fix backend startup

--- database.py
+++ database.py
@@ -7,6 +7,6 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "postgresql://postgres:1q2w3e@postgres_db:5432/todo_db"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
