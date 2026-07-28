a9d29eca79fe69026627106e7273ba127f0f195f
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-07-28 11:20:11 +0300

    added timeout for db connection

--- database.py
+++ database.py
@@ -7,6 +7,6 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "postgresql://postgres:1q2w3e@postgres_db:5432/todo_db"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, connect_args={"connect_timeout: 5"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
